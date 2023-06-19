39cfff5d627ce9da47321636198fcf84be1b7ac2
vika084 <59125867+vem24@users.noreply.github.com>
2023-06-19 04:51:36 -0700

    To be or not to be.

--- june_ex.js
+++ june_ex.js
@@ -1,3 +1,4 @@
+// Counter Function
 var createCounter = function(n) {
     return function() {
         return n++;
@@ -9,3 +10,29 @@ const counter = createCounter(10)
 counter() // 10
 counter() // 11
 counter() // 12
+
+/************************************/
+
+// Expect To Be or Not To Be
+var expect = function(val) {
+    return {
+        toBe: (value) => {
+            if (val !== value) {
+                throw new Error("Not Equal");
+            } else {
+                return true;
+            }
+        },
+        notToBe: (value) => {
+            if (val === value) {
+                throw new Error("Equal");
+            } else {
+                return true;
+            }
+        }
+    }
+};
+
+
+expect(5).toBe(5); // true
+expect(5).notToBe(5); // throws "Equal"
